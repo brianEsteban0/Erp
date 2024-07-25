ddf70aaeabe0408b3cc2c0cb7571156f74a38297
DanielBeltranSs <daniel.beltran2001@alumnos.ubiobio.cl>
2024-07-25 02:56:29 -0400

    estilos asistencia

--- frontend/src/components/AttendanceForm.jsx
+++ frontend/src/components/AttendanceForm.jsx
@@ -152,129 +152,177 @@ const AttendanceForm = () => {
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      <div className="p-5 relative border rounded flex flex-col items-center">
-        <h2 className="text-2xl font-bold mb-4 text-center text-black">Registrar Asistencia</h2>
-        <div className="w-full">
-          <label className="block mb-2 text-black">RUT del Empleado:</label>
-          <input
-            type="text"
-            value={rut}
-            onChange={(e) => setRut(e.target.value)}
-            required
-            className="p-2 mb-4 text-black border border-gray-300 rounded w-full"
-          />
-        </div>
-        {user.roles.some(role => role.name === 'admin') && (
-          <div className="mb-4 flex items-center">
-            <label className="mr-2 text-black">Registro manual:</label>
-            <label className="switch">
-              <input 
-                type="checkbox" 
-                checked={overrideAdmin} 
-                onChange={() => setOverrideAdmin(!overrideAdmin)} 
-              />
-              <span className="slider round"></span>
-            </label>
-          </div>
-        )}
-        <div className="mb-4 flex justify-center w-full">
-          <button onClick={handleCheckIn} className="custom-button bg-blue-500 text-white px-4 py-2 mr-2">Registrar Check-in</button>
-          <button onClick={handleCheckOut} className="custom-button bg-red-500 text-white px-4 py-2">Registrar Check-out</button>
-        </div>
-        {isWaiting && (
-          <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 rounded">
-            <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent border-t-4 border-t-blue-600 rounded-full animate-spin"></div>
-            <p className="mt-4 text-white">Por favor, ingrese su huella digital...</p>
-          </div>
-        )}
+      <div className="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10">
+        <table className="w-full table-fixed">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Registrar Asistencia</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            <tr>
+              <td className="py-4 px-6 border-b border-gray-200">
+                <div className="w-full">
+                  <label className="block mb-2 text-black">RUT del Empleado:</label>
+                  <input
+                    type="text"
+                    value={rut}
+                    onChange={(e) => setRut(e.target.value)}
+                    required
+                    className="p-2 mb-4 text-black border border-gray-300 rounded w-full"
+                  />
+                </div>
+                {user.roles.some(role => role.name === 'admin') && (
+                  <div className="mb-4 flex items-center">
+                    <label className="mr-2 text-black">Registro manual:</label>
+                    <label className="switch">
+                      <input 
+                        type="checkbox" 
+                        checked={overrideAdmin} 
+                        onChange={() => setOverrideAdmin(!overrideAdmin)} 
+                      />
+                      <span className="slider round"></span>
+                    </label>
+                  </div>
+                )}
+                <div className="mb-4 flex justify-center w-full">
+                  <button onClick={handleCheckIn} className="custom-button bg-blue-500 text-white px-4 py-2 mr-2">Registrar Check-in</button>
+                  <button onClick={handleCheckOut} className="custom-button bg-red-500 text-white px-4 py-2">Registrar Check-out</button>
+                </div>
+                {isWaiting && (
+                  <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 rounded">
+                    <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent border-t-4 border-t-blue-600 rounded-full animate-spin"></div>
+                    <p className="mt-4 text-white">Por favor, ingrese su huella digital...</p>
+                  </div>
+                )}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <div className="p-5 border rounded flex flex-col items-center">
-        <h2 className="text-2xl font-bold mb-4 text-center text-black">Último Registro</h2>
-        <div className="bg-white p-4 rounded shadow-md text-black text-center w-full">
-          {lastAttendance.photoUrl && (
-            <div className="mb-4">
-              <img src={getPhotoUrl(lastAttendance.photoUrl)} alt="Foto del usuario" className="w-24 h-24 rounded-full mx-auto" />
-            </div>
-          )}
-          <p><strong>RUT:</strong> {rut}</p>
-          <p><strong>Check-in:</strong> {formatDateTime(lastAttendance.checkIn)}</p>
-          <p><strong>Check-out:</strong> {formatDateTime(lastAttendance.checkOut)}</p>
-        </div>
+      <div className="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10">
+        <table className="w-full table-fixed">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Último Registro</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            <tr>
+              <td className="py-4 px-6 border-b border-gray-200">
+                <div className="bg-white p-4 rounded shadow-md text-black text-center w-full">
+                  {lastAttendance.photoUrl && (
+                    <div className="mb-4">
+                      <img src={getPhotoUrl(lastAttendance.photoUrl)} alt="Foto del usuario" className="w-24 h-24 rounded-full mx-auto" />
+                    </div>
+                  )}
+                  <p><strong>RUT:</strong> {rut}</p>
+                  <p><strong>Check-in:</strong> {formatDateTime(lastAttendance.checkIn)}</p>
+                  <p><strong>Check-out:</strong> {formatDateTime(lastAttendance.checkOut)}</p>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
       {user.roles.some(role => role.name === 'admin') && (
         <>
-          <div className="p-5 border rounded flex flex-col items-center col-span-1">
-            <h2 className="text-2xl font-bold mb-4 text-center text-black">Registros de Asistencia</h2>
-            <div className="w-full mb-4">
-              <label className="block mb-2 text-black">Buscar por RUT:</label>
-              <input
-                type="text"
-                value={searchRut}
-                onChange={(e) => setSearchRut(e.target.value)}
-                className="p-2 mb-4 text-black border border-gray-300 rounded w-full"
-              />
-            </div>
-            <div className="w-full mb-4">
-              <label className="block mb-2 text-black">Fecha de Inicio:</label>
-              <input
-                type="date"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-                className="p-2 mb-4 text-black border border-gray-300 rounded w-full"
-              />
-            </div>
-            <div className="w-full mb-4">
-              <label className="block mb-2 text-black">Fecha de Fin:</label>
-              <input
-                type="date"
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)}
-                className="p-2 mb-4 text-black border border-gray-300 rounded w-full"
-              />
-            </div>
-            <div className="flex justify-center w-full">
-              <button onClick={fetchAttendanceRecords} className="custom-button bg-green-500 text-white px-4 py-2 mb-4 mr-2">Buscar Registros</button>
-              <button onClick={fetchWorkHours} className="custom-button bg-blue-500 text-white px-4 py-2 mb-4">Horas Trabajadas</button>
-            </div>
-            <div className="w-full overflow-y-auto h-64">
-              {attendanceRecords.length > 0 ? (
-                <table className="min-w-full bg-white">
-                  <thead>
-                    <tr>
-                      <th className="py-2 text-black">Fecha</th>
-                      <th className="py-2 text-black">Check-in</th>
-                      <th className="py-2 text-black">Check-out</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {attendanceRecords.map((record) => (
-                      <tr key={record._id}>
-                        <td className="py-2 text-black">{formatDateTime(record.date)}</td>
-                        <td className="py-2 text-black">{formatDateTime(record.checkIn)}</td>
-                        <td className="py-2 text-black">{formatDateTime(record.checkOut)}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              ) : (
-                <p className="text-black">No se encontraron registros</p>
-              )}
-            </div>
+          <div className="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10 col-span-1">
+            <table className="w-full table-fixed">
+              <thead>
+                <tr className="bg-gray-100">
+                  <th className="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Registros de Asistencia</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                <tr>
+                  <td className="py-4 px-6 border-b border-gray-200">
+                    <div className="w-full mb-4">
+                      <label className="block mb-2 text-black">Buscar por RUT:</label>
+                      <input
+                        type="text"
+                        value={searchRut}
+                        onChange={(e) => setSearchRut(e.target.value)}
+                        className="p-2 mb-4 text-black border border-gray-300 rounded w-full"
+                      />
+                    </div>
+                    <div className="w-full mb-4">
+                      <label className="block mb-2 text-black">Fecha de Inicio:</label>
+                      <input
+                        type="date"
+                        value={startDate}
+                        onChange={(e) => setStartDate(e.target.value)}
+                        className="p-2 mb-4 text-black border border-gray-300 rounded w-full"
+                      />
+                    </div>
+                    <div className="w-full mb-4">
+                      <label className="block mb-2 text-black">Fecha de Fin:</label>
+                      <input
+                        type="date"
+                        value={endDate}
+                        onChange={(e) => setEndDate(e.target.value)}
+                        className="p-2 mb-4 text-black border border-gray-300 rounded w-full"
+                      />
+                    </div>
+                    <div className="flex justify-center w-full">
+                      <button onClick={fetchAttendanceRecords} className="custom-button bg-green-500 text-white px-4 py-2 mb-4 mr-2">Buscar Registros</button>
+                      <button onClick={fetchWorkHours} className="custom-button bg-blue-500 text-white px-4 py-2 mb-4">Horas Trabajadas</button>
+                    </div>
+                    <div className="w-full overflow-y-auto h-64">
+                      {attendanceRecords.length > 0 ? (
+                        <table className="min-w-full bg-white">
+                          <thead>
+                            <tr>
+                              <th className="py-2 text-black">Fecha</th>
+                              <th className="py-2 text-black">Check-in</th>
+                              <th className="py-2 text-black">Check-out</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {attendanceRecords.map((record) => (
+                              <tr key={record._id}>
+                                <td className="py-2 text-black">{formatDateTime(record.date)}</td>
+                                <td className="py-2 text-black">{formatDateTime(record.checkIn)}</td>
+                                <td className="py-2 text-black">{formatDateTime(record.checkOut)}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      ) : (
+                        <p className="text-black">No se encontraron registros</p>
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <div className="p-5 border rounded flex flex-col items-center col-span-1">
-            <h2 className="text-2xl font-bold mb-4 text-center text-black">Horas Trabajadas</h2>
-            {workUserInfo && workUserInfo.photoUrl && (
-              <div className="mb-4">
-                <img src={getPhotoUrl(workUserInfo.photoUrl)} alt="Foto del usuario" className="w-24 h-24 rounded-full mx-auto" />
-              </div>
-            )}
-            <div className="bg-white p-4 rounded shadow-md text-black text-center w-full">
-              <p><strong>RUT:</strong> {workUserInfo.rut}</p>
-              <p><strong>Nombre:</strong> {workUserInfo.username}</p>
-              <p><strong>Email:</strong> {workUserInfo.email}</p>
-              <p><strong>Horas Trabajadas:</strong> {workHours ? `${workHours} horas` : 'N/A'}</p>
-            </div>
-            <button onClick={generatePDF} className="custom-button bg-blue-500 text-white px-4 py-2 mt-4">Generar Informe</button>
+          <div className="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10 col-span-1">
+            <table className="w-full table-fixed">
+              <thead>
+                <tr className="bg-gray-100">
+                  <th className="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Horas Trabajadas</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                <tr>
+                  <td className="py-4 px-6 border-b border-gray-200">
+                    {workUserInfo && workUserInfo.photoUrl && (
+                      <div className="mb-4">
+                        <img src={getPhotoUrl(workUserInfo.photoUrl)} alt="Foto del usuario" className="w-24 h-24 rounded-full mx-auto" />
+                      </div>
+                    )}
+                    <div className="bg-white p-4 rounded shadow-md text-black text-center w-full">
+                      <p><strong>RUT:</strong> {workUserInfo.rut}</p>
+                      <p><strong>Nombre:</strong> {workUserInfo.username}</p>
+                      <p><strong>Email:</strong> {workUserInfo.email}</p>
+                      <p><strong>Horas Trabajadas:</strong> {workHours ? `${workHours} horas` : 'N/A'}</p>
+                    </div>
+                    <button onClick={generatePDF} className="custom-button bg-blue-500 text-white px-4 py-2 mt-4">Generar Informe</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </>
       )}
